2210ac7892479b6902695c6aa8ccabc86c8a622b
JSeam <joshiaseam@gmail.com>
2017-02-12 16:07:46 +0800

    Week2 Cartesian to Spherical stuff

--- Code/Week2.py
+++ Code/Week2.py
@@ -1,27 +1,36 @@
 from __future__ import print_function
-#import numpy
+import numpy as np
 import unittest
-#import scipy.constants as c
+import scipy.constants as c
 
 """
 Testing Framework with unittest
 """
 class codeTester(unittest.TestCase):
-    def qn1(self):
-    	self.assertEqual(energy_n(1), -13.60569)
-	self.assertEqual(energy_n(2), -3.40142)
-	self.assertEqual(energy_n(3), -1.51174)
+    def test_qn1(self):
+        self.assertEqual(energy_n(1), -13.60569)
+        self.assertEqual(energy_n(2), -3.40142)
+        self.assertEqual(energy_n(3), -1.51174)
 
-    def qn2(self):
-    	self.assertEqual(deg_to_rad(90), 1.5708)
-	self.assertEqual(deg_to_rad(180), 3.14159)
-	self.assertEqual(deg_to_rad(270), 4,71239)
-	self.assertEqual(rad_to_deg(3.14), 179.90875)
-	self.assertEqual(rad_to_deg(3.14/2.0), 89.95437)
-	self.assertEqual(rad_to_deg(3.14*3/4), 134.93156)
+    def test_qn2(self):
+        self.assertEqual(degToRad(90), 1.5708)
+        self.assertEqual(degToRad(180), 3.14159)
+        self.assertEqual(degToRad(270), 4.71239)
+        self.assertEqual(radToDeg(3.14), 179.90875)
+        self.assertEqual(radToDeg(3.14/2.0), 89.95437)
+        self.assertEqual(radToDeg(3.14*3/4), 134.93156)
+
+    def test_qn3(self):
+        self.assertEqual(sphericalToCartesian(3, 0, np.pi), (-0.0, 0.0, 3.0))
+        self.assertEqual(sphericalToCartesian(3, np.pi/2.0, np.pi/2.0), (0.0, 3.0, 0.0))
+        self.assertEqual(sphericalToCartesian(3, np.pi, 0), (0.0, 0.0, -3.0))
+        self.assertEqual(cartesianToSpherical(3, 0, 0), (3.0, 1.5708, 0.0))
+        self.assertEqual(cartesianToSpherical(0, 3, 0), (3.0, 1.5708, 1.5708))
+        self.assertEqual(cartesianToSpherical(0, 0, 3), (3.0, 0.0, 0.0))
+        self.assertEqual(cartesianToSpherical(0, -3, 0), (3.0, 1.5708, -1.5708))
+
 
 # Question 1
-
 def energy_n(n):
     """
     Create a function to calculate the energy level of a given
@@ -38,9 +47,8 @@ def energy_n(n):
     assert type(n) == int
     return round(-13.60569/n**2, 5)
 
-
 # Question 2
-def deg_to_rad(deg):
+def degToRad(deg):
     """
     Convert deg to rad. 5 decimal places output
 
@@ -54,7 +62,7 @@ def deg_to_rad(deg):
     assert type(deg) == float
     return round(deg*3.14159265359/180, 5)
 
-def rad_to_deg(rad):
+def radToDeg(rad):
     """
     Convert rad to deg. 5 decimal places output
 
@@ -63,13 +71,39 @@ def rad_to_deg(rad):
     """
     # Convert to float if int
     if type(rad) == int:
-    	rad = float(rad)
+        rad = float(rad)
     
     assert type(rad) == float
     return round(rad * 180 / 3.14159265359, 5)
+
+# Question 3
+def sphericalToCartesian(r, theta, phi):
+    '''
+    convert spherical coord to cartesian coord
+    :param r: radius
+    :param theta: theta angle
+    :param phi: phi angle
+    :return: (x, y, z) tuple of floats
+    '''
+
+    x = float(r) * np.sin(theta) * np.cos(phi)
+    y = float(r) * np.sin(theta) * np.sin(phi)
+    z = float(r) * np.cos(theta)
+
+    return (round(x,5), round(y,5), round(z,5))
+
+def cartesianToSpherical(x, y, z):
+    r = round(np.sqrt(x**2 + y**2 + z**2),5)
+    theta = round(np.arctan2(y,x),5)
+    phi = round(np.arctan2(np.sqrt(x**2+y**2),z), 5)
+
+    return (r, phi, theta)
+
 
 
 
 # Run the testing framework to verify the code
 if __name__ == "__main__":
-    unittest.main()    
+    #unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(codeTester)
+    unittest.TextTestRunner(verbosity=3).run(suite)
